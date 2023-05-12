05adb352c978d8f3abf38145db0ce606b0ef6d2a
EpicIbby101 <kevin_grittner@outlook.com>
2023-05-12 09:56:29 +0100

    Made the timezoneForm more responsive

--- src/pages/timezone.js
+++ src/pages/timezone.js
@@ -8,7 +8,7 @@ const TimezonePage = () => {
     <div className="flex min-h-fit flex-col justify-between text-center">
       <Header />
         <Image src="/images/earth.png" alt="Background" className="z-10" fill />
-      <div className="flex flex-col items-center text-center  p-16 h-full font-mono z-20">
+      <div className="flex flex-col items-center text-center  p-10 sm:p-16 h-full font-mono z-20">
         <h1 className="text-2xl sm:text-4xl font-bold mb-4">Find the Best Time To Talk</h1>
         <TimezoneForm />
       </div>
